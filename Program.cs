bf7cbab4fea0c1254a85f7cb2b2ebdd9a89e0c14
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-07 22:26:06 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_ParsingAndTryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
